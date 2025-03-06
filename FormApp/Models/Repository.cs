0f9e19f1bef28a95049accf0abd3cad108a5c3ee
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-03-06 20:37:37 +0300

    FormApp eklenmesi

---
+++ FormApp/Models/Repository.cs
@@ -0,0 +1,29 @@
+﻿namespace FormApp.Models
+{
+    public class Repository
+    {
+        private static readonly List<Product> _productList = new();
+        private static readonly List<Category> _categories = new();
+
+        static  Repository()
+        {
+            _categories.Add()
+        }
+        
+
+        public static List<Product> Products
+        {
+            get
+            {
+                return _productList;
+            }
+        }
+        public static List<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+        }
+    }
+}
---
+++ FormAppProduct/Models/Product.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FormAppProduct.Models
+{
+    public class Product
+    {
+        [Display(Name="Urun Id")]
+        public int ProductId { get; set; }
+        
+        [Required(ErrorMessage = "Gerekli bir alan")]
+        [StringLength(100)]
+        [Display(Name="Urun Adı")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(0, 100000)]
+        [Display(Name="Fiyat")]
+        public decimal? Price { get; set; }
+
+        [Display(Name="Resim")]
+        public string? Image { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+
+        [Display(Name="Category")]
+        
+        [Required]
+        public int? CategoryId { get; set; }
+
+    }
+}
+
+// 1 iphone 14 1
+// 2 iphone 15 1
