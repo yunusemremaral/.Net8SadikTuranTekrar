e7ec0cc63d9d229aec7835fdf834dd4fd14c7873
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-03-14 14:52:04 +0300

    BlogApp entity ve db baglantıları yapıldı.

---
+++ BlogApp/Program.cs
@@ -0,0 +1,37 @@
+using BlogApp.Data.Db;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BlogContext>(options =>
+{
+    var config = builder.Configuration;
+    var con = config.GetConnectionString("sqlite_connection");
+    options.UseSqlite(con);
+});
+var app = builder.Build();
+
+SeedData.TestVerileriniDoldur(app);
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
