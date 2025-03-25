1d4fe4b3fab611cd486d5a1752bee895cab75c24
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-03-26 00:50:21 +0300

    Session öncesi bitim card kısımları bitti ...

--- StoreApp.Web/Program.cs
+++ StoreApp.Web/Program.cs
@@ -5,37 +5,35 @@ using StoreApp.Web.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
 builder.Services.AddAutoMapper(typeof(MapperProfile).Assembly);
 
-
-builder.Services.AddDbContext<StoreDbContext>(options => {
-    options.UseSqlite(builder.Configuration["ConnectionStrings:StoreDbConnection"]);
+builder.Services.AddDbContext<StoreDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:StoreDbConnection"], b => b.MigrationsAssembly("StoreApp.Web"));
 });
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<Cart>(sc => SessionCart.GetCart(sc));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseStaticFiles();
+app.UseSession();
+
+// products/telefon => kategori urun listesi
 app.MapControllerRoute("products_in_category", "products/{category?}", new { controller = "Home", action = "Index" });
+
+// samsung-s24 => urun detay
 app.MapControllerRoute("product_details", "{name}", new { controller = "Home", action = "Details" });
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.Run();
