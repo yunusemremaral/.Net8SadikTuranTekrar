0d0c5bc55a2a08f4d6f937fbda0ea344d65aca87
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-03-25 21:53:27 +0300

    Pagination  yapımı ve tag helper

--- StoreApp.Web/Models/ProductViewModel.cs
+++ StoreApp.Web/Models/ProductViewModel.cs
@@ -11,6 +11,14 @@
     public class ProductListViewModel
     {
         public IEnumerable<ProductViewModel> Products { get; set; } = Enumerable.Empty<ProductViewModel>();
+        public PageInfo PageInfo { get; set; } = new();
 
+    }
+
+    public class PageInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
     }
 }
